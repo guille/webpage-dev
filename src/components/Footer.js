c4b238cad6aa6f0e2db113d32daa60c808e6c01a
guille <guille@users.noreply.github.com>
2022-12-03 18:51:51 +0100

    Open links in new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
           const { icon, name, url } = social;
           return (
             <li key={url}>
-              <a href={url} className={`icon ${icon}`}>
+              <a href={url} target="_blank" rel="noopener" className={`icon ${icon}`}>
                 <span className="label">{name}</span>
               </a>
             </li>
